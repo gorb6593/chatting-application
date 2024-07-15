8fe3c6d97c969edea5e2438ee62b64b573e09fbb
haekyu <gorb6593@gmail.com>
2024-07-15 17:49:47 +0900

    고양이 슈팅게임 추가

--- src/main/java/org/example/chatchatpractice/controller/ChatController.java
+++ src/main/java/org/example/chatchatpractice/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.example.chatchatpractice.service.ChatMessageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,6 +17,11 @@ public class ChatController {
     public String chat(Model model) {
         model.addAttribute("messages", chatMessageService.getAllMessages());
         return "chat";
+    }
+
+    @GetMapping("/shooting")
+    public String shooting() {
+        return "shooting";
     }
 
 }
