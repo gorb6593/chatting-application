9383a1c7126d82393961a2ab312fb3307c019d61
Harry <gorb6593@gmail.com>
2024-11-07 15:16:49 +0900

    커밋테스트3

--- src/main/java/org/example/chatchatpractice/controller/ProfileController.java
+++ src/main/java/org/example/chatchatpractice/controller/ProfileController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import java.security.Principal;
 
 @Slf4j
